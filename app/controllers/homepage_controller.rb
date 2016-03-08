85057a8307c85c082fe8f0afbf26f0453a1a2f27
Hitesh Sancheti <hitesh99@gmail.com>
2016-03-07 21:18:05 -0500

    fixed the show post issue

---
+++ app/controllers/homepage_controller.rb
@@ -0,0 +1,5 @@
+class HomepageController < ApplicationController
+	def show
+		 render  "homepage"
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :posts
-  root 'posts#index'
-
+  root 'homepage#show'
+  get "/homepage/" => "homepage#show"
 
 end
